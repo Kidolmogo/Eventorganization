c441d5922b96960982bbab63d99130378a4ed225
Kidolmogo <142190063+Kidolmogo@users.noreply.github.com>
2024-02-24 15:08:57 +0300

    Add files via upload

---
+++ onscroll.js
@@ -0,0 +1,20 @@
+document.addEventListener("readystatechange", (event) => {
+  if (event.target.readyState === "complete") {
+    initApp();
+  }
+});
+
+const initApp = () => {
+  const obserever = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      } else {
+        entry.target.classList.remove("show");
+      }
+    });
+  });
+
+  const hiddenElements = document.querySelectorAll(".hidden");
+  hiddenElements.forEach((el) => obserever.observe(el));
+};
